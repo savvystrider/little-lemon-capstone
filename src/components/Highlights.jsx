fa673e7918986bbc23d55f71f33d8a05c77bd006
Manish <mjpatel84@gmail.com>
2023-11-29 07:08:08 -0500

    adds Highlights content

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,3 +1,29 @@
 export default function Highlights() {
-  return <h1>Highlights</h1>;
+  return (
+    <section className="highlights">
+      <h2>This week's specials!</h2>
+      <button className="btn menu-btn">Online menu</button>
+      <div className="card-container">
+        <div className="card">
+          <div className="img-container">
+            <img
+              src="assets/greek-salad.jpg"
+              alt="Greek salad"
+              className="card-img"
+            />
+          </div>
+          <div className="card-row">
+            <h3>Greek salad</h3>
+            <p className="dish-price">$12.99</p>
+            <p className="dish-description">
+              The famous Greek salad made with crispy lettuce, peppers, olives,
+              and our homemade Chicago style feta cheese, garnished with crunch
+              garlic and rosemary croutons.
+            </p>
+            <p>Order delivery</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
