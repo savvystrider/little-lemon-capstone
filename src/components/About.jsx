1fd32788931eaf81dfb87cac8ffc0beb1414eb3b
Manish <mjpatel84@gmail.com>
2023-11-29 07:40:31 -0500

    adds basic content and structure for About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,15 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <section className="about">
+      <h2>Little Lemon</h2>
+      <p>Chicago</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+        veritatis quod similique cum officia iure neque impedit aspernatur
+        ratione in.
+      </p>
+      <img src="assets\Mario and Adrian A.jpg" alt="" />
+      <img src="assets\Mario and Adrian A.jpg" alt="" />
+    </section>
+  );
 }
