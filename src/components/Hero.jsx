7c83b4e61ad09ae99ee591597291fac3e925a56c
Manish <mjpatel84@gmail.com>
2023-11-29 07:02:55 -0500

    adds hero content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,20 @@
 export default function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <section className="hero">
+      <h1>Little Lemon</h1>
+      <p className="subtitle">Chicago</p>
+      <p className="description">
+        We are a family-owned Mediterranean resturant, focused on traditional
+        recipes served with a modern twist.
+      </p>
+      <div className="img-container">
+        <img
+          src="assets/restaurantfood.jpg"
+          alt="chef holding plate of food"
+          className="hero-img"
+        />
+      </div>
+      <button className="btn reserve-btn">Reserve a Table</button>
+    </section>
+  );
 }
