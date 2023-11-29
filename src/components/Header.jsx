13f3fdf6db233e58ecdad2c0979f8ead4c5dbffd
Manish <mjpatel84@gmail.com>
2023-11-29 07:00:00 -0500

    adds Header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,19 @@
+// import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <h1>header</h1>;
+  return (
+    <header>
+      <img src="assets/Logo.svg" alt="Little Lemon logo" className="logo" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Reservations</li>
+          <li>Order Online</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
