277a0120857fb1ae9b4a226ae8a599b9448c237a
Manish <mjpatel84@gmail.com>
2023-11-30 08:10:37 -0500

    adjusts Hero component and adds basic styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,24 @@
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>Little Lemon</h1>
-      <p className="subtitle">Chicago</p>
-      <p className="description">
-        We are a family-owned Mediterranean resturant, focused on traditional
-        recipes served with a modern twist.
-      </p>
-      <div className="img-container">
-        <img
-          src="assets\restauranfood.jpg"
-          alt="chef holding plate of food"
-          className="hero-img"
-        />
+      <div className="wrapper">
+        <div className="hero-text">
+          <h1>Little Lemon</h1>
+          <p className="subtitle">Chicago</p>
+          <p className="description">
+            We are a family-owned Mediterranean resturant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button className="btn reserve-btn">Reserve a Table</button>
+        </div>
+        <div className="img-container">
+          <img
+            src="assets\restauranfood.jpg"
+            alt="chef holding plate of food"
+            className="hero-img"
+          />
+        </div>
       </div>
-      <button className="btn reserve-btn">Reserve a Table</button>
     </section>
   );
 }
