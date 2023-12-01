880562f5cd614b70edbeaa0103a797df0e7fbba7
Manish <mjpatel84@gmail.com>
2023-12-01 06:08:44 -0500

    updates Testimonials and adds base styles

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,16 +6,16 @@ export default function Testimonials() {
       <div className="testimonial">
         <h2>Rosana Alexandersen</h2>
         <img
-          src="https://i.pravatar.cc/150?img=5"
+          src="https://i.pravatar.cc/125?img=5"
           alt="Rosana Alexandersen"
           className="avatar"
         />
         <div className="star-container">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
         </div>
         <p className="quote">
           Little Lemon is the best Mediterranean restaurant in Chicago!
@@ -24,16 +24,16 @@ export default function Testimonials() {
       <div className="testimonial">
         <h2>Eivor Varinsdottir</h2>
         <img
-          src="https://i.pravatar.cc/150?img=48"
+          src="https://i.pravatar.cc/125?img=48"
           alt="Eivor Varinsdottir"
           className="avatar"
         />
         <div className="star-container">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
         </div>
         <p className="quote">
           Can't get enough of the hummus and lemon desserts! Looking forward to
@@ -43,16 +43,16 @@ export default function Testimonials() {
       <div className="testimonial">
         <h2>Adele Castillo</h2>
         <img
-          src="https://i.pravatar.cc/150?img=31"
+          src="https://i.pravatar.cc/125?img=31"
           alt="Adele Castillo"
           className="avatar"
         />
         <div className="star-container">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
         </div>
         <p className="quote">
           This food is amazing! Can't recommend the salads enough.
@@ -61,16 +61,16 @@ export default function Testimonials() {
       <div className="testimonial">
         <h2>Rhys Warszawski</h2>
         <img
-          src="https://i.pravatar.cc/150?img=67"
+          src="https://i.pravatar.cc/125?img=67"
           alt="Rhys Warszawski"
           className="avatar"
         />
         <div className="star-container">
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
-          <AiFillStar />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
+          <AiFillStar style={{ fill: "#F4CE14" }} />
         </div>
         <p className="quote">
           If you're looking for authentic and tasty Mediterranean food, look no
