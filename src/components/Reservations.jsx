256307019ff58f7e37bb9fa8c36a7279a6bb2dbb
Manish <mjpatel84@gmail.com>
2023-12-02 08:41:09 -0500

    adds Reservations form

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,3 +1,36 @@
 export default function Reservations() {
-  return <div>Reservations page</div>;
+  return (
+    <div className="reservations">
+      <h2>Reservations</h2>
+      <form>
+        <div className="input-element">
+          <label htmlFor="date">Choose date</label>
+          <input type="date" id="date" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="time">Choose time</label>
+          <select name="time" id="time">
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+            <option value="22:00">22:00</option>
+          </select>
+        </div>
+        <div className="input-element">
+          <label htmlFor="guests">Number of Guests</label>
+          <input type="number" id="guests" placeholder="1" min={1} max={10} />
+        </div>
+        <div className="input-element">
+          <label htmlFor="occassion">Occassion</label>
+          <select name="occassion" id="occassion">
+            <option value="Birthday">Birthday</option>
+            <option value="Anniversary">Anniversary</option>
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
