75dfcba380159e854b9a463fcf036162c0c18539
Manish <mjpatel84@gmail.com>
2023-12-02 05:04:45 -0500

    updates footer content and adds styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,61 @@
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <nav>
+        <ul className="navigation">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Reservations</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+        <ul className="contact">
+          <li>
+            <a href="#">Address</a>
+          </li>
+          <li>
+            <a href="#">Phone Number</a>
+          </li>
+          <li>
+            <a href="#">Email</a>
+          </li>
+        </ul>
+        <ul className="social-media">
+          <li>
+            <a href="#"></a>
+            <AiFillFacebook />
+          </li>
+          <li>
+            <a href="#">
+              <AiFillInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <AiFillTwitterCircle />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  );
 }
