0f4d6542f71da30f65eb71f8180a46efbc53a7b2
Manish <mjpatel84@gmail.com>
2023-12-02 08:34:08 -0500

    updates Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Highlights from "../components/Highlights";
 import Testimonials from "../components/Testimonials";
 import About from "../components/About";
-import Footer from "../components/Footer";
 
 export default function Home() {
   return (
