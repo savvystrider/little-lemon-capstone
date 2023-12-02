f1c188a8d0503761f719797032809eafcc3978a2
Manish <mjpatel84@gmail.com>
2023-12-02 04:50:00 -0500

    updates About and adds styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,26 @@
 export default function About() {
   return (
     <section className="about">
-      <h2>Little Lemon</h2>
-      <p>Chicago</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
-        veritatis quod similique cum officia iure neque impedit aspernatur
-        ratione in.
-      </p>
-      <img src="assets\Mario and Adrian A.jpg" alt="" />
-      <img src="assets\Mario and Adrian b.jpg" alt="" />
+      <div className="about-text">
+        <h2>Little Lemon</h2>
+        <p className="subtitle">Chicago</p>
+        <p className="description">
+          We are a family-owned Mediterranean resturant, focused on traditional
+          recipes served with a modern twist.
+        </p>
+      </div>
+      <div className="img-top">
+        <img
+          src="assets\Mario and Adrian A.jpg"
+          alt="Little Lemon chefs, Mario and Adrian"
+        />
+      </div>
+      <div className="img-bottom">
+        <img
+          src="assets\Mario and Adrian b.jpg"
+          alt="Little Lemon chefs, Mario and Adrian"
+        />
+      </div>
     </section>
   );
 }
